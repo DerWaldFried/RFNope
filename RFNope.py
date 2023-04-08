54e9793ac7823e0327e5046e38c7772fbef8b651
DerWaldFried <patrick.b-privat@outlook.de>
2023-04-08 23:04:53 +0200

    Initial commit

---
+++ RFNope.py
@@ -0,0 +1,48 @@
+import os
+import bweb
+from rich.console import Console
+from rich.theme import Theme
+
+# change hosts path according to your OS
+hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
+
+custom_theme = Theme({
+    "info": "dim cyan",
+    "warning": "magenta",
+    "danger": "bold red"
+})
+console = Console(theme=custom_theme)
+console.print("[warning]RFNope[/warning]")
+console.print("This Software will Block the German RundFunk and his Services!", style="danger")
+
+accept = console.input("Enter [warning]YES[/warning] when you want Block all Websites and Services!"
+                       "Type NO when you want do delete the Block of all Rundfunk Services\n")
+# When User Choose YES
+if accept == "YES":
+    if os.name == "nt":
+        console.print("You have accepted with [warning]YES[/warning], we will now Block")
+        # Open Host Data in Write mode
+        with open(hosts_path, 'a') as hosts_file:
+
+            # We Block now all Sites
+            for url in bweb.blocked_urls:
+                hosts_file.write('127.0.0.1 {}\n'.format(url))
+
+        # Reload the DNS-Cache to load blocked List
+        os.system('ipconfig /flushdns')
+
+# When User Choose NO
+elif accept == "NO":
+    console.print("You have Choose NO. The Software will now delete all Blocked Website and Services")
+    # Reading the Content of Host Data in Write mode
+    with open(hosts_path, 'r') as hosts_file:
+        hosts_contents = hosts_file.readlines()
+
+    # Writing new Host Data without blocking
+    with open(hosts_path, 'w') as hosts_file:
+        for line in hosts_contents:
+            if not any(url in line for url in bweb.blocked_urls):
+                hosts_file.write(line)
+    os.system('ipconfig /flushdns')
+else:
+    console.print("You Dont have Choose YES or NO. The Software has do nothing")
---
+++ bweb.py
@@ -0,0 +1,16 @@
+# Liste der URLs, die blockiert werden sollen
+blocked_urls = ['www.3sat.de', 'www.ardalpha.de',
+                'www.arte.tv', 'www.br.de', 'www.daserste.de',
+                'www.dw.com', 'www.hr-fernsehen.de', 'www.kika.de',
+                'www.mdr.de', 'www.ndr.de', 'www.ardmediathek.de',
+                'www.phoenix.de', 'www.radiobremen.de', 'www.rbb-online.de',
+                'www.sr.de', 'www.swrfernsehen.de', 'www.ard.de', 'www1.wdr.de'
+                'www.wdr.de', 'www.zdf.de', 'www.ardmediathek.de', 'www.funk.net',
+                'www.deutschlandfunk.de', 'www.deutschlandfunkkultur.de', 'www.deutschlandfunknova.de',
+                'www.br-klassik.de', 'www.hr1.de', 'www.hr2.de', 'www.hr3.de', 'www.hr4.de',
+                'www.hr-inforadio.de', 'www.you-fm.de', 'www.bremeneins.de', 'www.radiobremen.de',
+                'www.bremennext.de', 'www.bremenvier.de', 'www.n-joy.de', 'www.unserding.de',
+                'www.dasding.de', 'ardaudiothek.de', 'www.youtube.com/@funk', 'www.youtube.com/c/TRUDOKU',
+                'www.tiktok.com/@trudoku', 'www.instagram.com/trudoku/', 'www.youtube.com/channel/UCZHpIFMfoJJ_1QxNGLJTzyA',
+                'www.youtube.com/mailab', 'www.youtube.com/ykollektiv', 'www.instagram.com/y_kollektiv']
+
