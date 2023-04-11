7a6faf624c5e1bac79ea931cba4d6ba8f15671e7
DerWaldFried <patrick.b-privat@outlook.de>
2023-04-11 22:01:10 +0200

    Added better Way for start Functions.

--- RFNope.py
+++ RFNope.py
@@ -1,44 +1,42 @@
 import os
-import bweb
 from rich.console import Console
 from rich.theme import Theme
 
-import ntsys
-import pssys
-
-# change hosts path according to your OS
-hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
 
 custom_theme = Theme({
-    "info": "dim cyan",
-    "warning": "magenta",
-    "danger": "bold red"
-})
+        "info": "dim cyan",
+        "warning": "magenta",
+        "danger": "bold red"
+    })
+
 console = Console(theme=custom_theme)
 console.print("[warning]RFNope[/warning]")
 console.print("This Software will Block the German RundFunk and his Services!", style="danger")
 
-accept = console.input("Enter [warning]YES[/warning] when you want Block all Websites and Services!"
-                       "Type NO when you want do delete the Block of all Rundfunk Services\n")
+console.print("Enter [warning]YES[/warning] when you want Block all Websites and Services!\n")
+console.print(" Type NO when you want do delete the Block of all Rundfunk Services\n")
+accept = console.input()
+
 # When User Choose YES
 if accept == "YES":
     if os.name == "nt":
-        ntsys.NTBlocker()
-    if os.name == "posix":
-        pssys.PSBlocker()
+        from ntsys import ntblocker
+        console.print("NTBLocker will called")
+        ntblocker()
+    else:
+        import pssys
+
+        pssys.Main.PSBlocker()
 
 # When User Choose NO
-elif accept == "NO":
-    console.print("You have Choose NO. The Software will now delete all Blocked Website and Services")
-    # Reading the Content of Host Data in Write mode
-    with open(hosts_path, 'r') as hosts_file:
-        hosts_contents = hosts_file.readlines()
+if accept == "NO":
+    if os.name == "nt":
+        from ntsys import ntdeblocker
+        ntdeblocker()
+    else:
+        import pssys
 
-    # Writing new Host Data without blocking
-    with open(hosts_path, 'w') as hosts_file:
-        for line in hosts_contents:
-            if not any(url in line for url in bweb.blocked_urls):
-                hosts_file.write(line)
-    os.system('ipconfig /flushdns')
+        pssys.PSDeblocker()
+
 else:
     console.print("You Dont have Choose YES or NO. The Software has do nothing")
