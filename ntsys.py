15c4ec045fe794e16525fcdfd968718c61a08a71
DerWaldFried <patrick.b-privat@outlook.de>
2023-04-11 21:59:32 +0200

    -Added Admin Privilegs and rework code. classes are now functions

--- ntsys.py
+++ ntsys.py
@@ -1,21 +1,49 @@
+import pyuac
 import os
+
 from rich.console import Console
+
 import bweb
 
 
-class NTBlocker:
-    # change hosts path according to your OS
-    hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
+def ntblocker():
+    if not pyuac.isUserAdmin():
+        print("Re-launching as admin!")
+        pyuac.runAsAdmin()
+    else:
+        # change hosts path according to your OS
+        hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
 
-    console = Console
+        console = Console()
 
-    console.print("You have accepted with [warning]YES[/warning], we will now Block")
-    # Open Host Data in Write mode
-    with open(hosts_path, 'a') as hosts_file:
+        console.print("You have accepted with [warning]YES[/warning], we will now Block")
+        # Open Host Data in Write mode
+        with open(hosts_path, 'a') as hosts_file:
 
-        # We Block now all Sites
-        for url in bweb.blocked_urls:
-            hosts_file.write('127.0.0.1 {}\n'.format(url))
+            # We Block now all Sites
+            for url in bweb.blocked_urls:
+                hosts_file.write('127.0.0.1 {}\n'.format(url))
 
-    # Reload the DNS-Cache to load blocked List
-    os.system('ipconfig /flushdns')
+        # Reload the DNS-Cache to load blocked List
+        os.system('ipconfig /flushdns')
+
+
+def ntdeblocker():
+    if not pyuac.isUserAdmin():
+        print("Re-launching as admin!")
+        pyuac.runAsAdmin()
+    else:
+        hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
+        console = Console()
+
+        console.print("You have Choose NO. The Software will now delete all Blocked Website and Services")
+        # Reading the Content of Host Data in Write mode
+        with open(hosts_path, 'r') as hosts_file:
+            hosts_contents = hosts_file.readlines()
+
+        # Writing new Host Data without blocking
+        with open(hosts_path, 'w') as hosts_file:
+            for line in hosts_contents:
+                if not any(url in line for url in bweb.blocked_urls):
+                    hosts_file.write(line)
+        os.system('ipconfig /flushdns')
